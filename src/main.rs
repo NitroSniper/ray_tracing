d216313ec126ce5c615578d02a63af2d312dc845
NitroSniper <mahiemiah35@gmail.com>
2024-12-05 12:22:57 +0000

    Camera struct

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,29 @@ use winit_input_helper::WinitInputHelper;
 
 static ASPECT_RATIO: f64 = 16.0 / 9.0;
 static IMAGE_WIDTH: u32 = 256;
-static IMAGE_HEIGHT: u32 = {
-    let height = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-    if height == 0 {
-        1
-    } else {
-        height
-    }
-};
+
 fn main() {
     // calculate image height
+
+    // World
+    let sphere_1 = Sphere {
+        center: Vector3::new(0.0, 0.0, -1.0),
+        radius: 0.5,
+    };
+    let sphere_2 = Sphere {
+        center: Vector3::new(0.0, -100.5, -1.0),
+        radius: 100.0,
+    };
+    let world = vec![sphere_1, sphere_2];
 
     // image
     let event_loop = EventLoop::new().unwrap();
     let _input = WinitInputHelper::new();
     event_loop.set_control_flow(ControlFlow::Poll);
+    let camera = Camera::new(ASPECT_RATIO, IMAGE_WIDTH);
 
     let window = {
-        let size = LogicalSize::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+        let size = LogicalSize::new(camera.image_width as u32, camera.image_height as u32);
         WindowBuilder::new()
             .with_title("Hell")
             .with_inner_size(size)
@@ -42,7 +47,7 @@ fn main() {
         let window_size = window.inner_size();
         dbg!(window_size);
         let surf = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(IMAGE_WIDTH, IMAGE_HEIGHT, surf).unwrap()
+        Pixels::new(camera.image_width as u32, camera.image_height as u32, surf).unwrap()
     };
 
     let _ = event_loop.run(|event, elwt| match event {
@@ -54,63 +59,15 @@ fn main() {
             event: WindowEvent::RedrawRequested,
             ..
         } => {
-            draw_image(pixels.frame_mut());
+            camera.render(pixels.frame_mut(), &world);
             pixels.render().unwrap();
             window.request_redraw();
-        },
+        }
         _ => (),
     });
 }
 
-fn draw_image(frame: &mut [u8]) {
-    // World
-    let sphere_1 = Sphere {center: Vector3::new(0.0, 0.0, -1.0), radius: 0.5};
-    let sphere_2 = Sphere {center: Vector3::new(0.0, -100.5, -1.0), radius: 100.0};
-    let world = vec![sphere_1, sphere_2];
-
-    // Camera
-    let vp_height = 2.0;
-    let vp_width = vp_height * (IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64);
-    let focal_length = 1.0;
-    let camera_center = Vector3::from_value(0.0);
-
-    // Calculate the vectors across the horizontal and down the vertical view edges.
-    let vp_u = Vector3::unit_x() * vp_width;
-    let vp_v = Vector3::unit_y() * -vp_height;
-
-    // calculate delta between pixel
-    let pixel_delta_u = vp_u / IMAGE_WIDTH as f64;
-    let pixel_delta_v = vp_v / IMAGE_HEIGHT as f64;
-
-    // calculate the location of the top left pixel
-    let top_left_pixel = camera_center - Vector3::unit_z() * focal_length - vp_u / 2.0 - vp_v / 2.0;
-    let pixel00_loc = top_left_pixel + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-
-    for (i, pixels) in frame.chunks_exact_mut(4).enumerate() {
-        let x = i % IMAGE_WIDTH as usize;
-        let y = i / IMAGE_WIDTH as usize;
-        let pixel_center = pixel00_loc + pixel_delta_u * x as f64 + pixel_delta_v * y as f64;
-        let ray = Ray::new(camera_center, pixel_center - camera_center);
-        let color = ray_color(ray, &world);
-        let u8_color = color.map(|x| (x * 255.0).round() as u8);
-        pixels.copy_from_slice(
-            &[u8_color.x, u8_color.y, u8_color.z, 0xff]
-        );
-    }
-}
-
 type Color = Vector3<f64>;
-fn ray_color<T: Hitter>(ray: Ray<f64>, hittables: &[T]) -> Color {
-    if let Some(record) = hittables.hit(&ray, &(0.0..f64::infinity())) {
-        return 0.5 * (record.normal + Vector3::from_value(1.0))
-    }
-    let unit_dir = ray.dir.normalize();
-    let a = 0.5 * (unit_dir.y + 1.0);
-    (1.0 - a) * Vector3::from_value(1.0) + a * Vector3::new(0.5, 0.7, 1.0)
-}
-
-
 
 struct HitRecord {
     point: Vector3<f64>,
@@ -122,21 +79,24 @@ struct HitRecord {
 impl HitRecord {
     fn set_face_normal(&mut self, ray: &Ray<f64>, out_norm: &Vector3<f64>) {
         self.front_face = ray.dir.dot(*out_norm) < 0.0;
-        self.normal = if self.front_face {*out_norm} else {-1.0 * out_norm};
+        self.normal = if self.front_face {
+            *out_norm
+        } else {
+            -1.0 * out_norm
+        };
     }
 }
 
 impl Default for HitRecord {
     fn default() -> Self {
-        Self { 
-            point: Vector3::new(0.0,0.0,0.0),
-            normal: Vector3::new(0.0,0.0,0.0),
+        Self {
+            point: Vector3::new(0.0, 0.0, 0.0),
+            normal: Vector3::new(0.0, 0.0, 0.0),
             t: 0.0,
             front_face: false,
         }
     }
 }
-
 
 trait Hitter {
     fn hit(&self, ray: &Ray<f64>, t: &Range<f64>) -> Option<HitRecord>;
@@ -153,15 +113,15 @@ impl Hitter for Sphere {
         let a = ray.dir.dot(ray.dir);
         let h = ray.dir.dot(oc);
         let c = oc.dot(oc) - self.radius * self.radius;
-        let discriminant = h*h - a*c;
+        let discriminant = h * h - a * c;
         if discriminant < 0.0 {
-            return None
+            return None;
         }
         let sqrtd = discriminant.sqrt();
         let root = {
-            let root_a = (h-sqrtd) / a;
-            let root_b = (h+sqrtd) / a;
-            if t.contains(&root_a){
+            let root_a = (h - sqrtd) / a;
+            let root_b = (h + sqrtd) / a;
+            if t.contains(&root_a) {
                 Some(root_a)
             } else if t.contains(&root_b) {
                 Some(root_b)
@@ -174,10 +134,9 @@ impl Hitter for Sphere {
         record.point = ray.at(root);
         let out_normal = (record.point - self.center) / self.radius;
         record.set_face_normal(ray, &out_normal);
-        Some(record) 
+        Some(record)
     }
 }
-
 
 impl<T: Hitter> Hitter for [T] {
     fn hit(&self, ray: &Ray<f64>, t: &Range<f64>) -> Option<HitRecord> {
@@ -195,4 +154,76 @@ impl<T: Hitter> Hitter for [T] {
     }
 }
 
+struct Camera {
+    aspect_ratio: f64,
+    image_width: u32,
+    image_height: u32,
+    center: Vector3<f64>,
+    pixel00_loc: Vector3<f64>,
+    pixel_delta_u: Vector3<f64>,
+    pixel_delta_v: Vector3<f64>,
+}
 
+impl Camera {
+    fn new(aspect_ratio: f64, image_width: u32) -> Self {
+        let image_height = {
+            let height = (image_width as f64 / aspect_ratio) as u32;
+            if height == 0 {
+                1
+            } else {
+                height
+            }
+        };
+
+        let f_width = image_width as f64;
+        let f_height = image_height as f64;
+
+        let vp_height = 2.0;
+        let vp_width = vp_height * (f_width / f_height);
+        let focal_length = 1.0;
+        let center = Vector3::from_value(0.0);
+
+        // Calculate the vectors across the horizontal and down the vertical view edges.
+        let vp_u = Vector3::unit_x() * vp_width;
+        let vp_v = Vector3::unit_y() * -vp_height;
+
+        // calculate delta between pixel
+        let pixel_delta_u = vp_u / f_width;
+        let pixel_delta_v = vp_v / f_height;
+
+        // calculate the location of the top left pixel
+        let top_left_pixel = center - Vector3::unit_z() * focal_length - vp_u / 2.0 - vp_v / 2.0;
+        let pixel00_loc = top_left_pixel + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        Self {
+            aspect_ratio,
+            image_width,
+            image_height,
+            center,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
+    }
+    fn ray_color<T: Hitter>(ray: Ray<f64>, hittables: &[T]) -> Color {
+        if let Some(record) = hittables.hit(&ray, &(0.0..f64::infinity())) {
+            return 0.5 * (record.normal + Vector3::from_value(1.0));
+        }
+        let unit_dir = ray.dir.normalize();
+        let a = 0.5 * (unit_dir.y + 1.0);
+        (1.0 - a) * Vector3::from_value(1.0) + a * Vector3::new(0.5, 0.7, 1.0)
+    }
+
+    fn render<T: Hitter>(&self, frame: &mut [u8], world: &[T]) {
+        for (i, pixels) in frame.chunks_exact_mut(4).enumerate() {
+            let x = i % IMAGE_WIDTH as usize;
+            let y = i / IMAGE_WIDTH as usize;
+            let pixel_center =
+                self.pixel00_loc + self.pixel_delta_u * x as f64 + self.pixel_delta_v * y as f64;
+            let ray = Ray::new(self.center, pixel_center - self.center);
+            let color = Camera::ray_color(ray, world);
+            let u8_color = color.map(|x| (x * 255.0).round() as u8);
+            pixels.copy_from_slice(&[u8_color.x, u8_color.y, u8_color.z, 0xff]);
+        }
+    }
+}
