f4c0215cca9e488f1efefef827ef694c12093ae9
NitroSniper <mahiemiah35@gmail.com>
2025-05-28 12:15:46 +0100

    Multiple Sphere with coloured Normals

--- gpu-implementation/src/cuda/ray.cu
+++ gpu-implementation/src/cuda/ray.cu
@@ -1,9 +1,14 @@
-__device__ float3 ray_color(Ray r, Sphere& s) {
+__device__ float3 ray_color(Ray r, Sphere* world, unsigned int world_size) {
     float2 t = make_float2(0.0f, 1024.0f);
-    HitRecord record = s.hit(r, t);
-    if (!record.is_none) {
-        float3 n = normalize(sub(r.at(record.t), make_float3(0.0, 0.0, -1.0)));
-        return mul(make_float3(n.x+1.0, n.y+1.0,n.z+1.0), 0.5);
+
+    // float3 color = make_float3(1.0f, 1.0f, 1.0f);
+    for (int i = 0; i < world_size; i++) {
+        Sphere& s = world[i];
+        HitRecord record = s.hit(r, t);
+        if (!record.is_none) {
+            float3 n = normalize(sub(r.at(record.t), make_float3(0.0, 0.0, -1.0)));
+            return mul(make_float3(n.x+1.0, n.y+1.0,n.z+1.0), 0.5);
+        }
     }
 
     float3 unit_dir = normalize(r.dir);
@@ -40,20 +45,23 @@ __device__ uchar4 to_pixel(float4 fpixel) {
 
 
 extern "C" __global__ void render(uint64_t *rng_state, uchar4 *const frame, Camera cam_g, GuiState gui_g) {
+    // Load shared data
     __shared__ Camera cam;
     __shared__ Sphere s;
     __shared__ GuiState gui;
+    const unsigned int world_size = 2;
+    __shared__ Sphere world[world_size];
     cam = static_cast<Camera&&>(cam_g);
     gui = static_cast<GuiState&&>(gui_g);
-    s = Sphere(make_float3(0.0f,0.0f,-1.0f), 0.5f);
+    world[0] = Sphere(make_float3(0.0f,0.0f,-1.2f), 0.5f);
+    world[1] = Sphere(make_float3(0.0f, -100.5f,-1.0f), 100.0f);
     __syncthreads();
+
+    // if thread doesn't have a pixel
 	unsigned int idx = 1024*blockIdx.x + threadIdx.x;
     if (idx >= cam.image_width*cam.image_height) return;
-    unsigned int samples = gui.sample2_per_pixel*gui.sample2_per_pixel;
 
 	pcg32_global = {rng_state[idx], 0};
-// 	Camera cam2 = static_cast<Camera&&>(cam);
-
 
     if (gui.show_random) {
 	    float3 rgb = gui.random_norm ? random_norm_float3() : random_float3();
@@ -65,8 +73,9 @@ extern "C" __global__ void render(uint64_t *rng_state, uchar4 *const frame, Came
 	int i = idx % cam.image_width;
 	int j = idx / cam.image_width;
     float3 total = make_float3(0.0f, 0.0f, 0.0f);
+    unsigned int samples = gui.sample2_per_pixel*gui.sample2_per_pixel;
     for (int sample_idx = 0; sample_idx < samples; sample_idx++) {
-        total = add(total, ray_color(get_ray(i, j, sample_idx, gui.sample2_per_pixel, cam), s));
+        total = add(total, ray_color(get_ray(i, j, sample_idx, gui.sample2_per_pixel, cam), world, world_size));
         __syncthreads();
     }
 
